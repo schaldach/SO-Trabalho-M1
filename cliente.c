333de05934a8d00f41275e9978874091fe34dc0b
Gabriel Schaldach Morgado <72992857+schaldach@users.noreply.github.com>
2025-04-05 17:58:37 +0000

    setup codigo

--- cliente.c
+++ cliente.c
@@ -1,1 +1,9 @@
+#include	<pthread.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<unistd.h>
+
+int main( int argc, char *argv[]) {
+
+}
 
--- servidor.c
+++ servidor.c
@@ -1,1 +1,8 @@
+#include	<pthread.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<unistd.h>
 
+int main( int argc, char *argv[]) {
+
+}
