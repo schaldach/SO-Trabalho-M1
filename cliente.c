2045535df2293cc565543ce0a7ca26d399d4f562
Jonas <127670304+JonasRH355@users.noreply.github.com>
2025-04-14 00:11:12 +0000

    Lógica de verificar se a string é vazia E loop do cliente [CLIENTE]

--- cliente.c
+++ cliente.c
@@ -8,22 +8,37 @@
  #include "banco.h"
  
  int main(){
-    int fd1;
-    mkfifo(myfifo, 0666);
-    
-    char test_string[QUERY_SIZE];
-    fgets(test_string, QUERY_SIZE, stdin);
+   int fd1;
+   char test_string[QUERY_SIZE];
+   mkfifo(myfifo, 0666);
 
-    // a diferença entre fgets e scanf é que fgets retorna o '\n' também, o que
-    // é ruim, pois o '\n' já é adicionado no banco (necessário para se a string passar do limite)
-    // e queremos o banco organizado, então vamos tirar o '\n'
-    // (isso foi movido para o lado do servidor, para deixar o cliente mais livre)
-    printf("%s\n", test_string);
+   while(1){
+      fd1 = -1;
+      fgets(test_string, QUERY_SIZE, stdin);
 
-    fd1 = open(myfifo, O_WRONLY);
-    write(fd1, test_string, strlen(test_string)+1);
+      // a diferença entre fgets e scanf é que fgets retorna o '\n' também, o que
+      // é ruim, pois o '\n' já é adicionado no banco (necessário para se a string passar do limite)
+      // e queremos o banco organizado, então vamos tirar o '\n'
+      // (isso foi movido para o lado do servidor, para deixar o cliente mais livre)
+      
+      //Verifica se foi inserido algum valor
+      if(strcmp(test_string,"\n") == 0){
+         printf("[Error] Please enter a command:\n");
+         
+      }else if(strcmp(test_string,"sair\n") == 0){ //caso foi inserido "sair", o cliente será finalizado
+         break;
+      }
+      else{ // Executar a atividade
+         printf("\"%s\"\n", test_string);
 
-    close(fd1);
+         fd1 = open(myfifo, O_WRONLY);
+         write(fd1, test_string, strlen(test_string)+1);
+         
+         close(fd1);
+      
+      }
+         
+   }
 
-    return 0;
+   return 0;
  }
